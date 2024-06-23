905f2b6181649186addea00b6ce23ecb048cbd65
Suchi-2024 <suchintikayetheko@gmail.com>
2024-06-23 20:50:14 +0530

    Password Reset Added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import warnings
 from dashboard import main
 from about import about
 from verify import is_verification_expired
+from firebase_admin import credentials, auth
 import time
 import json
 
@@ -30,11 +31,23 @@ firebaseConfig = {
 
 #Firebase Authentication
 firebase=pyrebase.initialize_app(firebaseConfig)
-auth=firebase.auth()
+auth2=firebase.auth()
+
+
+# Initialize the Firebase Admin SDK (if not already initialized)
+# try:
+#     firebase_admin.get_app()
+# except ValueError:
+#     try:
+#         cred = credentials.Certificate("source.json")
+#         firebase_admin.initialize_app(cred, {'projectId': 'heart-disease-predictor-730c3'})
+#     except Exception as e:
+#         print("Error initializing Firebase Admin SDK:", e)
 
 #Database
 db=firebase.database()
 storage=firebase.storage()
+
 
 
 st.sidebar.title(":orange[Choose Menu]")
@@ -45,31 +58,77 @@ st.sidebar.title(":orange[Choose Menu]")
 def clear_query_params():
     #st.experimental_set_query_params()
     st.query_params.clear()
+# Initialize session state variables
+if 'email' not in st.session_state:
+    st.session_state['mail'] = ''
+if 'password' not in st.session_state:
+    st.session_state['pwd'] = ''
+if 'username' not in st.session_state:
+    st.session_state['username'] = ''
+if 'parameters' not in st.session_state:
+    st.session_state['parameters'] = {}
+if 'submitted' not in st.session_state:
+    st.session_state['submitted'] = False
+if 'clear' not in st.session_state:
+    st.session_state['clear'] = False
+
 def submit_action(valid_email, valid_pwd, valid_name):
     st.session_state['parameters'] = {'email': valid_email, 'password': valid_pwd, 'username': valid_name}
     st.session_state['submitted'] = True
+    username_exists = False  # Initialize the variable here
     try:
-                
-        user=auth.create_user_with_email_and_password(valid_email,valid_pwd)
-        user = auth.refresh(user['refreshToken'])
-        link = auth.send_email_verification(user['idToken'])            
-        #st.write(f'Verification link: {link}')
-        st.info('Please verify your email to Log In!')
-        st.success("Account Created Successfully!")
-        st.balloons()
+        users = db.child("").get().val()  # Assuming your users are stored under the root node of your database
+        if users:
+            for user_id, user_data in users.items():
+                username = user_data.get("UserName")
+                if username == valid_name:
+                    username_exists = True
+                    break
+        if username_exists:
+            st.error("Username already taken. Please choose a different username.")
+            #clear_inputs()  # Clear the inputs after successful submission
+            #st.experimental_rerun()
+        else:    
+            user=auth2.create_user_with_email_and_password(valid_email,valid_pwd)
+            user = auth2.refresh(user['refreshToken'])
+            link = auth2.send_email_verification(user['idToken'])            
+            st.info('Please verify your email to Log In!')
+            st.success("Account Created Successfully!")
+            st.balloons()
 
-        #Sign in
-        user=auth.sign_in_with_email_and_password(valid_email,valid_pwd)
-        db.child(user['localId']).child("UserName").set(valid_name)
-        db.child(user['localId']).child("ID").set(user['localId'])
+            #Sign in
+            user=auth2.sign_in_with_email_and_password(valid_email,valid_pwd)
+            db.child(user['localId']).child("UserName").set(valid_name)
+            db.child(user['localId']).child("ID").set(user['localId'])
 
-        st.title("Welcome "+valid_name)
-        st.info("👈 Log in to your account to get features.")
-        #st.experimental_rerun()
+            st.title("Welcome "+valid_name)
+            st.info("👈 Log in to your account to get features.")
+
+            st.session_state['clear'] = True  # Set clear flag  # Clear the inputs after successful submission
+            st.experimental_rerun()
     except requests.exceptions.HTTPError as e:
         st.error("User already exist.")
+         # Clear the input fields
+        st.session_state['clear'] = True  # Set clear flag # Clear the inputs after successful submission
+            
+            # Refresh the page to clear the inputs
         #st.experimental_rerun()
         time.sleep(2) 
+
+def is_valid_email(email):
+    try:
+        # Retrieve the list of users from Firebase Authentication
+        user_list = auth.list_users()
+
+        # Check if the email exists in the list of registered users
+        for user in user_list.iterate_all():
+            if user.email == email:
+                return True
+
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
 
 def validate_email(email):
     patt=r"[a-zA-Z0-9]+@+[a-zA-Z]+.[a-zA-Z]{2,}$"
@@ -108,25 +167,40 @@ def validate_name(name):
     else:
         return name
 
-def forgot_password():
-    email = st.text_input("Enter your email address to reset your password")
-    reset_button = st.button("Send Password Reset Email")
-
-    if reset_button:
+def forgot_password(email):
+    if is_valid_email(email):
         try:
-            auth.send_password_reset_email(email)
+            auth2.send_password_reset_email(email)
             st.success("Password reset email sent! Please check your inbox.")
+                #st.rerun()
         except requests.exceptions.HTTPError as e:
-            error_msg = e.response.json().get('error', {}).get('message', 'An error occurred.')
-            st.error(error_msg)
-            st.warning("Invalid email")
+            if e.response is not None:
+                if e.response.status_code == 400:
+                    error_msg = e.response.json()['error']['message']
+                    st.error(f"User not found: {error_msg}")
+                else:
+                    error_msg = e.response.json()['error']['message']
+                    st.error(f"An error occurred: {error_msg}")
+            else:
+                st.error("An error occurred during the password reset process.")
+    else:
+        st.warning("User Not Found")
+    # Clear all output
+ 
+        
 
 def signup():
     st.title("Sign Up")
+    if st.session_state['clear']:
+        st.session_state['mail'] = ''
+        st.session_state['pwd'] = ''
+        st.session_state['username'] = ''
+        st.session_state['clear'] = False  # Reset clear flag
 
-    email=st.text_input("Please enter your email id")
-    password=st.text_input("Password", type='password')
-    username=st.text_input("Please enter an user name")
+    
+    email = st.text_input("Enter your email id", key="mail")
+    password = st.text_input("Enter your Password", type='password', key="pwd")
+    username = st.text_input("Enter your Username", key="username")
     submit=st.button("Submit")
     valid_pwd=validate_password(password)
     valid_email=validate_email(email)
@@ -175,36 +249,112 @@ def signup():
             
            
             #login()
+def clear_text():
+    st.session_state["text"] = ""
 
+# def login():
+#     st.title("Log In your account")
+#     email = st.text_input("Enter your registered email id")
+#     password = st.text_input("Enter your Password", type='password')
+#     valid_pwd = validate_password(password)
+#     valid_email = validate_email(email)
+
+#     show_reset = st.session_state.get('show_reset', False)
+
+#     if st.button("Log In"):
+#         if len(email) == 0 and len(password) == 0:
+#             st.warning("Enter your email id and password")
+#         elif valid_pwd != password:
+#             st.warning("Correctly type your password")
+#         elif (valid_email) != email:
+#             st.warning("Correctly type your email id")
+#     # Define a session state variable to control the visibility of the reset password section
+#     if 'show_reset' not in st.session_state:
+#         st.session_state.show_reset = False
+
+#     forget = st.button("Forgot Password? 😟")
+
+#     if forget:
+#     # Toggle the visibility of the reset password section
+#         st.session_state.show_reset = not st.session_state.show_reset
+
+#     if st.session_state.show_reset:
+#         email_id = st.text_input("Enter your registered email id to reset password", value=st.session_state.get("text", ""))
+#         valid_email = validate_email(email_id)
+
+#         if st.button("Reset Password"):
+#             if len(valid_email) == 0:
+#                 st.warning("Please enter your email address to reset your password.")
+#             else:
+#                 forgot_password(valid_email)
+#             # Clear the input field after the button is clicked
+#                 email_id = ""
+#                 st.session_state.text = email_id
+
+#         else:
+            
+
+#             try:
+#                 user = auth2.sign_in_with_email_and_password(valid_email, valid_pwd)
+#                 user_response = auth2.get_account_info(user['idToken'])
+#                 users = user_response.get('users')
+#                 for user in users:
+#                     email_verify = (user.get("emailVerified"))
+#                     if email_verify:
+#                         name = db.child(user['localId']).child("UserName").get().val()
+#                         try:
+#                             st.query_params(name=name)
+#                         except:
+#                             pass
+#                             logged_in = True
+#                             st.session_state['logged_in'] = True
+#                             st.success("Credentials matched successfully")
+#                             st.info("Click Log In Button again")
+
+#                             return name
+#                     else:
+#                         st.warning(
+#                         "Verify your email at first.If you don't find an email, try to sign up with a valid email address.")
+
+#             except requests.exceptions.HTTPError as e:
+#                 if e.response is not None:
+#                     error_msg = e.response.json()['error']['message']
+#                     if 'TOO_MANY_ATTEMPTS_TRY_LATER' in error_msg:
+#                         st.error(
+#                             "Access to this account has been temporarily disabled due to many failed login attempts. Please reset your password or try again later.")
+#                     else:
+#                         st.error("An error occurred: {}".format(error_msg))
+#                 else:
+#                     #st.toast("Invalid Credentials")
+#                     time.sleep(2)
 
 def login():
     st.title("Log In your account")
-    email=st.text_input("Enter your registered email id")
-    password=st.text_input("Enter your Password", type='password')
-    valid_pwd=validate_password(password)
-    valid_email=validate_email(email)
-    btn1,btn2=st.columns(2)
-    submit=st.button("Log In")
-    if st.button("Forgot Password? 😟"):
-        forgot_password()
-    if len(email)==0 and len(password)==0:
-        st.warning("Enter your email id and password")
-    elif valid_pwd!=password:
-        st.warning("Correctly type your password")
-    elif (valid_email)!=email:
-        st.warning("Correctly type your password")
-    else:
-       
-        if submit:
+    email = st.text_input("Enter your registered email id")
+    password = st.text_input("Enter your Password", type='password')
+    valid_pwd = validate_password(password)
+    valid_email = validate_email(email)
+
+        # Initialize show_reset if not found in st.session_state
+    if 'show_reset' not in st.session_state:
+        st.session_state.show_reset = False
+
+    if st.button("Log In"):
+        if len(email) == 0 and len(password) == 0:
+            st.warning("Enter your email id and password")
+        elif valid_pwd != password:
+            st.warning("Correctly type your password")
+        elif (valid_email) != email:
+            st.warning("Correctly type your email id")
+        else:
             try:
-                user=auth.sign_in_with_email_and_password(valid_email,valid_pwd)
-                user_response =auth.get_account_info(user['idToken'])
+                user = auth2.sign_in_with_email_and_password(valid_email, valid_pwd)
+                user_response = auth2.get_account_info(user['idToken'])
                 users = user_response.get('users')
                 for user in users:
-                    email_verify=(user.get("emailVerified"))
+                    email_verify = (user.get("emailVerified"))
                     if email_verify:
                         name = db.child(user['localId']).child("UserName").get().val()
-                            # Redirect to dashboard with query parameter 'name'
                         try:
                             st.query_params(name=name)
                         except:
@@ -212,24 +362,43 @@ def login():
                         logged_in = True
                         st.session_state['logged_in'] = True
                         st.success("Credentials matched successfully")
-                        st.info("Click Log In Button again")    
-                            
+                        st.info("Click Log In Button again")
                         return name
                     else:
-                        st.warning("Verify your email at first.If you don't find an email, try to sign up with a valid email address.")
-                    
+                        st.warning(
+                            "Verify your email at first.If you don't find an email, try to sign up with a valid email address.")
+
             except requests.exceptions.HTTPError as e:
                 if e.response is not None:
                     error_msg = e.response.json()['error']['message']
                     if 'TOO_MANY_ATTEMPTS_TRY_LATER' in error_msg:
-                        st.error("Access to this account has been temporarily disabled due to many failed login attempts. Please reset your password or try again later.")
+                        st.error(
+                            "Access to this account has been temporarily disabled due to many failed login attempts. Please reset your password or try again later.")
                     else:
                         st.error("An error occurred: {}".format(error_msg))
                 else:
                     st.error("Invalid Credentials")
-                    #time.sleep(2)
-            #return ("Hello "+name)
-       
+
+    forget = st.button("Forgot Password? 😟")
+
+    if forget:
+        st.session_state.show_reset = not st.session_state.show_reset
+
+    if st.session_state.show_reset:
+        email_id = st.text_input("Enter your registered email id to reset password")
+        valid_email = validate_email(email_id)
+
+        if st.button("Reset Password"):
+            if len(valid_email) == 0:
+                st.warning("Please enter your email address to reset your password.")
+            else:
+                forgot_password(valid_email)
+                st.session_state.text = ""  # Clear the input field after the button is clicked
+
+        
+   
+
+              
 
 def logout():
     btn1,btn2,btn3,btn4,btn5,btn6=st.columns(6)
@@ -253,12 +422,14 @@ choice=st.sidebar.radio("Choose an option",("Log In","Sign Up","About","Home"))
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
-    
+if 'accname' not in st.session_state:
+    st.session_state['accname'] = None
+
 if st.session_state['logged_in']:
     clear_query_params()
     #st.write(f"Logged in as {st.session_state['username']}")
     if choice=="Home":
-        dashboard(st.session_state['username'])
+        dashboard(st.session_state['accname'])
         main()
 
     else:
@@ -271,7 +442,7 @@ if st.session_state['logged_in']:
             # st.title("Don't Have an account?")
             # submit=st.button("Create one")
             logout()
-            st.success(f"Already Logged In as {st.session_state['username']}")
+            st.success(f"Already Logged In as {st.session_state['accname']}")
             
             # if submit:
             #     signup()
@@ -304,7 +475,7 @@ else:
                 # st.session_state['logged_in'] = True
                 # st.session_state['username'] = name
                 #st.write("Press Log In Once again")
-            st.session_state['username'] = name
+            st.session_state['accname'] = name
     if choice=="Home":
         st.info("Log In First to get all the features!!")
     if choice=="About":
