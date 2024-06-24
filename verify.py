cefc77f97ce8fa8f07e291424cc97e55ca154af9
Suchi-2024 <suchintikayetheko@gmail.com>
2024-06-25 01:15:23 +0530

    Secured

--- verify.py
+++ verify.py
@@ -3,18 +3,10 @@ import firebase_admin
 import pyrebase
 from firebase_admin import credentials, firestore
 import time
+from fire import base
 
-firebaseConfig = {
- 'apiKey': "AIzaSyBnythJMbS6lnHqOAerJ6F0EBTrJylNeTY",
-  'authDomain': "heart-disease-predictor-730c3.firebaseapp.com",
-  'databaseURL': "https://heart-disease-predictor-730c3-default-rtdb.europe-west1.firebasedatabase.app",
-  'projectId': "heart-disease-predictor-730c3",
-  'storageBucket': "heart-disease-predictor-730c3.appspot.com",
-  'messagingSenderId': "740324649038",
-  'appId': "1:740324649038:web:2c0767947e27f92a3afb1f",
-  'measurementId': "G-M1RN5PLLXW"
-};
 
+firebaseConfig=base()
 #Firebase Authentication
 firebase=pyrebase.initialize_app(firebaseConfig)
 auth=firebase.auth()
