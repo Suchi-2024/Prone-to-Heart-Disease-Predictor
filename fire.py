ccd5134c428ec4a4a2e9b64c00396781ac1c8ce4
Suchi-2024 <suchintikayetheko@gmail.com>
2024-06-25 10:16:17 +0530

    Bug Resolved-2

---
+++ fire.py
@@ -0,0 +1,13 @@
+
+def base():
+    firebaseConfig = {
+    'apiKey': "AIzaSyBnythJMbS6lnHqOAerJ6F0EBTrJylNeTY",
+    'authDomain': "heart-disease-predictor-730c3.firebaseapp.com",
+    'databaseURL': "https://heart-disease-predictor-730c3-default-rtdb.europe-west1.firebasedatabase.app",
+    'projectId': "heart-disease-predictor-730c3",
+    'storageBucket': "heart-disease-predictor-730c3.appspot.com",
+    'messagingSenderId': "740324649038",
+    'appId': "1:740324649038:web:2c0767947e27f92a3afb1f",
+    'measurementId': "G-M1RN5PLLXW"
+    };
+    return firebaseConfig
